30145b26d01b74fc1100e4d3c1d523ae24c445ff
Daniil <dadyarri@gmail.com>
2023-12-12 00:51:01 +0300

    configure application

--- TheMessenger.Api/Program.cs
+++ TheMessenger.Api/Program.cs
@@ -1,9 +1,41 @@
+using FastEndpoints.Swagger;
+
+using Microsoft.AspNetCore.HttpOverrides;
+
+using Serilog;
+using Serilog.Events;
+
+using TheMessenger.Api.Extensions;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
+Log.Logger = new LoggerConfiguration()
+  .WriteTo.Console()
+  .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+  .MinimumLevel.Override("Default", LogEventLevel.Debug)
+  .MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Verbose)
+  .CreateLogger();
+
+
+builder.Host.UseSerilog();
+builder.ConfigureFastEndpoints();
+builder.ConfigureDatabase();
+builder.MapConfiguration();
+builder.RegisterDependencies();
+builder.ConfigureSwaggerDocuments();
 
 var app = builder.Build();
 
+app.UseForwardedHeaders(new ForwardedHeadersOptions {
+  ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+});
+
+app.UseSerilogRequestLogging();
+app.ConfigureAuthorization();
+app.ConfigureFastEndpoints();
+app.UseSwaggerGen(c => { c.Path = "/swagger/{documentName}/swagger.json"; });
+app.ApplyMigrations();
 
 app.UseHttpsRedirection();
 app.Run();
