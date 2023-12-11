2463500aea0a75e3e8fc3840352a863b840caead
Daniil <dadyarri@gmail.com>
2023-12-12 01:39:27 +0300

    add simple ping endpoint

---
+++ TheMessenger.Api/Endpoints/Ping.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+
+using FluentResults;
+
+using TheMessenger.Api.Configuration;
+
+
+namespace TheMessenger.Api.Endpoints;
+
+public class Ping : Endpoint<EmptyRequest, Result<string>> {
+  public override async Task<Result<string>> ExecuteAsync(EmptyRequest req, CancellationToken ct) {
+    return Result.Ok("pong");
+  }
+
+  public override void Configure() {
+    Get("/ping");
+    Summary(s => {
+      s.Summary = "Always returns pong";
+      s.Description = AllowedScopes.Anyone;
+    });
+    Options(o => o.WithTags(ApiTags.Other.Name));
+    AllowAnonymous();
+  }
+}
