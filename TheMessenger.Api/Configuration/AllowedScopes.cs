c88526015b243763dd2d0f61d209123e8e4782a3
Daniil <dadyarri@gmail.com>
2023-12-12 01:39:07 +0300

    add list of allowed scopes for endpoints

---
+++ TheMessenger.Api/Configuration/AllowedScopes.cs
@@ -0,0 +1,7 @@
+﻿namespace TheMessenger.Api.Configuration;
+
+public static class AllowedScopes {
+  public static readonly string Anyone = "<b>Allowed scopes:</b> Anyone, incl. anonymous";
+  public static readonly string Authorized = "<b>Allowed scopes:</b> Only autorized users";
+  
+}
