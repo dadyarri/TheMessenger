b1aa889ee105df9324590de52bb877c9ce12b3f3
Daniil <dadyarri@gmail.com>
2023-12-12 01:38:42 +0300

    add new api tag

--- TheMessenger.Api/Configuration/ApiTags.cs
+++ TheMessenger.Api/Configuration/ApiTags.cs
@@ -5,4 +5,5 @@ public record ApiTag(string Name, string Description);
 public class ApiTags {
   public static readonly ApiTag Messages = new("Messages", "API methods related to messages");
   public static readonly ApiTag Users = new("Users", "API methods related to users");
+  public static readonly ApiTag Other = new("Other", "API methods, that can't be related to any other category");
 }
