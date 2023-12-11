fe6319200908c12a67fec472d89abd92767b7fd8
Daniil <dadyarri@gmail.com>
2023-12-12 00:50:18 +0300

    add separate class for all api tags

---
+++ TheMessenger.Api/Configuration/ApiTags.cs
@@ -0,0 +1,8 @@
+﻿namespace TheMessenger.Api.Configuration;
+
+public record ApiTag(string Name, string Description);
+
+public class ApiTags {
+  public static readonly ApiTag Messages = new("Messages", "API methods related to messages");
+  public static readonly ApiTag Users = new("Users", "API methods related to users");
+}
